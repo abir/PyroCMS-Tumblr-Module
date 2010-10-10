134a747884e6e614daac44ed524ad9ef44e39d2e
Ben Edmunds <ben.edmunds@gmail.com>
2010-10-10 16:37:25 -0500

    updated sql and readme

--- tumblr/tumblr.sql
+++ tumblr/tumblr.sql
@@ -1,2 +1,4 @@
-INSERT INTO `settings` VALUES
+INSERT INTO `settings` 
+('slug','title','description','type','default','value','options','is_required','is_gui','module')
+VALUES
  ('tumblr_username','Tumblr Username','Username for Tumblr integration','text',NULL,NULL,'',0,0,'tumblr');
