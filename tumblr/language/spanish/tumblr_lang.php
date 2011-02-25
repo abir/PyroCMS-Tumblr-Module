36c8d3d5bf9d5bc904bae2ce6749126aae9ff8ea
Shemes <semrah@gmail.com>
2011-02-25 20:43:27 +0100

    adeded spanish languaje

---
+++ tumblr/language/spanish/tumblr_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['tumblr_form_title'] 							= 'Configuraci&oacute;n de Tumblr'; 
+$lang['tumblr_username_label'] 							= 'Nombre de usuario'; 
+$lang['tumblr_posted_label'] 							= 'Publicado'; 
+$lang['tumblr_posted_label_alt']						= 'Publicado en'; 
+$lang['tumblr_date_label'] 							= 'Fecha';
+$lang['tumblr_date_at']								= 'en'; 
+$lang['tumblr_view_label'] 							= 'Vista';
+$lang['tumblr_title_label'] 							= 'T&iacute;tulo';
+$lang['tumblr_slug_label'] 							= 'URL';
+$lang['tumblr_read_more_label'] 						= 'Leer mas&nbsp;&raquo;';
+$lang['tumblr_posts_title'] 							= 'Publicaciones';
+
+?>
